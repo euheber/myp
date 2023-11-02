5c2b6b41ed19db2f0e33215be260198a4a96a5a6
euheber <euheber1@gmail.com>
2023-11-02 14:49:56 -0300

    color pallete update

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,12 +11,11 @@ export default {
       'lora': [ 'Lora', 'serif'],
     },
     colors: { 
-      'f2c94d': '#eca531',
-      '3DC731': '#3DC731',
-      '1d1e20': '#1d1e20',
-      'e5e5e5': '#e5e5e5',
-      'e63946': '#e63946',
-      '1d3557': '#1d3557',
+      'freesia': '#FEC84D',
+      'black': '#1d1e20',
+      'white': '#ffffff',
+      'blue': '#4D76F2',
+      'blue-green': '#00B1B0',
     }
   },
   plugins: [],
