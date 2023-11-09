8453d54d796f0aa75df3a3aecfde5ed2acb27dc3
euheber <euheber1@gmail.com>
2023-11-09 20:12:07 -0300

    register form

--- src/views/register.jsx
+++ src/views/register.jsx
@@ -1,38 +1,97 @@
 function register() {
   return (
     <main className="mt-10 px-5 pb-2">
-      <p className="font-lorat text-xl">Antes de prosseguir, escolha se você</p>
+      <h1 className="font-lorat text-xl">
+        Cadastre a ONG para ter acesso a nossas ferramentas
+      </h1>
 
       <form className="mt-10 flex flex-col gap-5">
-        <details className="cursor-pointer">
-          <summary >Sobre a ong</summary>
+        <details className="border space-y-5">
+          <summary className="cursor-pointer">Sobre a ong</summary>
+
+          <div>
+            <label htmlFor="ongname" className="mr-3">
+              Nome da organização:
+            </label>
+            <input type="text" id="ongname" className="border" />
+          </div>
+
+          <div>
+            <label className="mr-3">Estado:</label>
+            <select>
+              <option value="sp">São Paulo</option>
+            </select>
+          </div>
+
+          <div>
+            <label className="mr-3">Cidade:</label>
+            <select>
+              <option value="cosmópolis">Cosmópolis</option>
+            </select>
+          </div>
+
+          <div>
+            <label htmlFor="street" className="mr-3">
+              Rua:
+            </label>
+            <input type="text" id="street" className="border" />
+          </div>
+
+          <div>
+            <label htmlFor="street" className="mr-3">
+              Número:
+            </label>
+            <input type="text" id="number" className="border" />
+          </div>
+
+          <div className="flex flex-col">
+            <label htmlFor="details" className="mr-3">
+              Um pouco sobre a ong
+            </label>
+            <textarea
+              name="details"
+              id="details"
+              cols="30"
+              rows="10"
+              className="border border-black rounded-md"
+            ></textarea>
+          </div>
+        </details>
+
+        <details className="space-y-5">
+          <summary className="cursor-pointer">Responsável</summary>
+
           <div>
             <label htmlFor="name" className="mr-3">
-              Nome da organização:
+              Nome
             </label>
             <input type="text" id="name" className="border" />
           </div>
 
           <div>
-            <label htmlFor="address" className="mr-3">
-              Endereço:
+            <label htmlFor="email" className="mr-3">
+              Email
             </label>
-            <input type="text" id="address" className="border" />
+            <input type="text" id="email" className="border" />
           </div>
 
-          
-          <div className="flex flex-col">
-            <label htmlFor="details" className="mr-3">
-              Um pouco sobre a ong
+          <div>
+            <label htmlFor="password" className="mr-3">
+              Senha
             </label>
-            <textarea name="details" id="details" cols="30" rows="10"></textarea>
+            <input type="text" id="email" className="border" />
+          </div>
+
+          <div className="flex flex-col gap-2">
+            <label htmlFor="profilepicture">Foto de perfil</label>
+            <input type="file" name="" id="" />
           </div>
         </details>
 
 
-        <details>
-            <summary>Sobre você</summary>
-        </details>
+        <button className="border border-black hover:text-freesia duration-150 transition w-56 mx-auto rounded-md p-2">
+            Cadastrar
+        </button>
       </form>
     </main>
   )
