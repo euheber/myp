5313816bf30e632283a3f81e39f091ef76677ce2
euheber <euheber1@gmail.com>
2023-11-14 23:09:15 -0300

    feat:

--- src/components/Inputs/InputsField.jsx
+++ src/components/Inputs/InputsField.jsx
@@ -1,16 +1,16 @@
 import { useState } from "react"
 
-function input({onInputChange, identity, type, label}) {
+function input({onInputChange, identity, type, label, name}) {
 
     const handleChange = (event) => { 
-        onInputChange(event.target.value)
+        onInputChange(event)
     }
   return (
     <div>
       <label htmlFor={identity} className="mr-3 block">
        {label}
       </label>
-      <input className="border border-black p-2 w-full rounded-md mt-2 mb-2" type={type} id={identity} onChange={handleChange}/>
+      <input className="border border-black p-2 w-full rounded-md mt-2 mb-2" type={type} id={identity} onChange={handleChange} name={name}/>
     </div>
   )
 }
--- src/views/register.jsx
+++ src/views/register.jsx
@@ -1,12 +1,20 @@
 import { useState } from "react"
 import InputField from "../components/Inputs/InputsField"
-import OptionsField from "../components/Inputs/OptionField"
+import OptionField from "../components/Inputs/OptionField"
 
 function register() {
-  const [meuValor, setMeuValor] = useState("")
+  const [meuValor, setMeuValor] = useState({ 
+    ongname: '',
+    neighborhood: '', 
+    street: '',
+    number: '',
+  })
 
-  const handleMeuValorChange = (novoValor) => {
-    setMeuValor(novoValor)
+  const handleMeuValorChange = (event) => {
+    setMeuValor({ 
+      ...meuValor,
+      [event.target.name]: event.target.value
+    })
   }
 
   return (
@@ -14,7 +22,7 @@ function register() {
       <h1 className="font-lorat text-xl">
         Cadastre a ONG para ter acesso a nossas ferramentas
       </h1>
-
+      {meuValor.ongname}
       <form className="mt-10 flex flex-col gap-5">
         <details className="border space-y-5">
           <summary className="cursor-pointer">Sobre a ong</summary>
@@ -24,6 +32,7 @@ function register() {
             type="text"
             identity={"ongname"}
             label={"Nome da Organização"}
+            name={'ongname'}
           />
 
           <InputField
@@ -31,22 +40,34 @@ function register() {
             type="text"
             identity={"street"}
             label={"Rua"}
+            name={'street'}
           />
+
+          <InputField
+            onInputChange={handleMeuValorChange}
+            type="text"
+            identity={"bairro"}
+            label={"Bairro"}
+            name={'neighborhood'}
+          />
+
+
           <InputField
             onInputChange={handleMeuValorChange}
             type="text"
             identity={"number"}
             label={"Número"}
+            name={'number'}
           />
 
           <div className="flex gap-4">
             <label>Estado/Cidade</label>
             <select>
-              <OptionsField value={"sp"} content={"São paulo"} />
+              <OptionField value={"sp"} content={"São paulo"} />
             </select>
 
             <select>
-              <OptionsField value={"cosmopolis"} content={"Cosmópolis"} />
+              <OptionField value={"cosmopolis"} content={"Cosmópolis"} />
             </select>
           </div>
         </details>
@@ -59,18 +80,21 @@ function register() {
             type={"text"}
             identity={"name"}
             label={"Nome"}
+            name={"name"}
           />
           <InputField
             onInputChange={handleMeuValorChange}
             type={"text"}
             identity={"email"}
             label={"Email"}
+            name={"email"}
           />
           <InputField
             onInputChange={handleMeuValorChange}
             type={"text"}
             identity={"password"}
             label={"Senha"}
+            name={"password"}
           />
         </details>
 
